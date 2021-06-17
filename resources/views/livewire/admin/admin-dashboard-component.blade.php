2ac095d24ccd656431f2aa8563a5a8c84d2423b9
Tauhidur Rahman <tauhid5051@gmail.com>
2021-06-17 11:04:44 +0600

    First commit

---
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Admin DashBoard</h1>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+</div>
---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>User Dashboard</h1>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+</div>
